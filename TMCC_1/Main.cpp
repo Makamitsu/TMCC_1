f7f4922b30e4a4456190ee30258dcf02a6d04640
Augustin Ambiehl <ambiehlaugustin@gmail.com>
2018-06-27 07:54:57 +0200

    [TMCC 1] end

--- TMCC_1/Main.cpp
+++ TMCC_1/Main.cpp
@@ -7,16 +7,12 @@ using namespace std;
 * Sum of all numbers multiple of 3 or 5 up to a given limit
 */
 
-int getSumOf3or5(int pMax) {
-	int sum = 0;
+unsigned long long getSumOf3or5(int pMax) {
+	unsigned long long sum = 0;
 	for (int i = 3; i <= pMax; i += 3)
-	{
 		sum += i;
-	}
 	for (int i = 5; i <= pMax; i += 5)
-	{
 		sum += i;
-	}
 	return sum;
 }
 
@@ -26,7 +22,7 @@ void main() {
 	cout << "******* EXERCICE 1 *******\n";
 	cout << "**************************\n";
 
-	int max;
+	unsigned long max;
 
 	cout << "Indiquez une limit sup:";
 	cin >> max;
